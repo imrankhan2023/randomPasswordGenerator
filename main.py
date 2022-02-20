34c66b3d59dcd2a0229df353a3f28d77d2280c87
ImRaN KhaN <90782350+imrankhan2023@users.noreply.github.com>
2022-02-20 23:21:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+length = int(input('Enter the length of password: \n'))
+all = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+temp = random.sample(all,length)
+
+# create the password
+password = "".join(temp)
+print(f'Password is : {password}')
+    
